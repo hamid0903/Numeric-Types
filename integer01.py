e7b6a1789f4845e1d7ecc68b224d8103fd0ac4c2
Khamid Kamalov <hamid_kamalov@mail.ru>
2021-11-01 20:52:45 +0500

    Num01

---
+++ integer01.py
@@ -0,0 +1,15 @@
+from math import sqrt
+
+a=2
+b=3
+
+#input(0<a<100)
+#input(0<b<1000)
+x=sqrt(sqrt((pow(a,3)-pow(b,3))+(pow(a,3)+pow(b,3))))
+print(type,(x))
+
+a=8
+b=5
+
+x=sqrt(sqrt((pow(a,3)-pow(b,3))+(pow(a,3)+pow(b,3))))
+print(type,(x))
