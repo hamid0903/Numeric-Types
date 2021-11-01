691a8b6f146fadffb318afc0c031318543f3ec60
Khamid Kamalov <hamid_kamalov@mail.ru>
2021-11-01 21:02:02 +0500

    Num07

---
+++ integer07.py
@@ -0,0 +1,19 @@
+from math import sqrt
+
+x1=-1.4
+y1=7.9
+x2=6.1
+y2=9.9
+
+c=sqrt(pow((x2-x1),2)+pow((y2-y1),2))
+x=round(c,2)
+print(x)
+
+x1=-3.9
+y1=-8.2
+x2=-1.3
+y2=-2.9
+
+c=sqrt(pow((x2-x1),2)+pow((y2-y1),2))
+x=round(c,2)
+print(x)
