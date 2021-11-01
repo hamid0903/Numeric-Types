d08b34dbfa3a52cf4278c84f52e5d8c3c1106fe0
Khamid Kamalov <hamid_kamalov@mail.ru>
2021-11-01 21:00:02 +0500

    Num05

---
+++ integer05.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+a=6.3
+b=8.6
+#0<a<100
+#0<b<1000
+c=9*pow(a,2)*b-27*pow(a,2)*pow(b,2)+15*pow(b,2)
+x=round(c,2)
+print(x)
+
+a=4.5
+b=9.6
+#0<a<100
+#0<b<1000
+c=9*pow(a,2)*b-27*pow(a,2)*pow(b,2)+15*pow(b,2)
+x=round(c,2)
+print(x)
